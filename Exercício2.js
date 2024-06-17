d16bcf62e5cacf86ea9b38a1478651c3a71fb545
Heleninha <helena_neis-silva@estudante.sesisenai.org.br>
2024-06-17 10:18:01 -0300

    ultima atualização

--- Exercício2.js
+++ Exercício2.js
@@ -1,18 +1,21 @@
-let res= document.getElementById('res')
+let res = document.getElementById('res')
 
-function calc_pre(){
+function calcula_pressao(){
     let pressao = Number(document.getElementById('pressao').value)
-    console.log(pressao)
-     
-    // Entrada de dados do teclado
-    
-    console.log("pressao =" + pressao)
+    let pressao2 = Number(document.getElementById('pressao2').value)
+
+    console.log("pressao = " + pressao + "pressao2 = " + pressao2)
 
     let conversao = pressao * 0.0193367801
     console.log("Em PSI fica = " + conversao + "PSI")
+    let conversao2 = pressao2 / 0.0193367801
+    console.log("Em PSI fica = " + conversao2 + "PSI")
 
-    res.innerHTML = "Em PSI fica = " + conversao.toFixed(2) + "PSI"
+    res.innerHTML = "Em PSI fica = " + conversao.toFixed(2) + " PSI"
+    res2.innerHTML = "Em mmHg fica = " + conversao2.toFixed(2) + " mmHg"
 
     res.style.fontSize = "1.25rem"
     res.style.fontWeight = "bold"
+    res2.style.fontSize = "1.25rem"
+    res2.style.fontWeight = "bold"
 }
